0f7a41543270c522fff6e177fb1f39923dfe39de
danieleangeletti <angelettidaniele@gmail.com>
2024-02-06 14:12:47 +0000

    ADD Bootstrap table - esercizio completato

--- index.php
+++ index.php
@@ -1,78 +1,102 @@
 <?php
 
-    $hotels = [
+$hotels = [
 
-        [
-            'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
-            'parking' => true,
-            'vote' => 4,
-            'distance_to_center' => 10.4
-        ],
-        [
-            'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 2
-        ],
-        [
-            'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
-            'parking' => false,
-            'vote' => 1,
-            'distance_to_center' => 1
-        ],
-        [
-            'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
-            'parking' => false,
-            'vote' => 5,
-            'distance_to_center' => 5.5
-        ],
-        [
-            'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 50
-       ],
+  [
+    'name' => 'Hotel Belvedere',
+    'description' => 'Hotel Belvedere Descrizione',
+    'parking' => true,
+    'vote' => 4,
+    'distance_to_center' => 10.4,
+  ],
+  [
+    'name' => 'Hotel Futuro',
+    'description' => 'Hotel Futuro Descrizione',
+    'parking' => true,
+    'vote' => 2,
+    'distance_to_center' => 2
+  ],
+  [
+    'name' => 'Hotel Rivamare',
+    'description' => 'Hotel Rivamare Descrizione',
+    'parking' => false,
+    'vote' => 1,
+    'distance_to_center' => 1
+  ],
+  [
+    'name' => 'Hotel Bellavista',
+    'description' => 'Hotel Bellavista Descrizione',
+    'parking' => false,
+    'vote' => 5,
+    'distance_to_center' => 5.5
+  ],
+  [
+    'name' => 'Hotel Milano',
+    'description' => 'Hotel Milano Descrizione',
+    'parking' => true,
+    'vote' => 2,
+    'distance_to_center' => 50
+  ],
 
-    ];
+];
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>PHP Hotel</title>
 
-    <!-- Bootstrap -->
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-    />
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>PHP Hotel</title>
 
-    <!-- CSS -->
-    <link rel="stylesheet" href="./css/style.css" />
-  </head>
-  <body>
-    <header>
-      <h1>PHP Hotels</h1>
-    </header>
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" />
 
-    <main>
-      <ul>
-        <?php
-          foreach ($hotels as $hotel){
-            echo "<li>Name: $hotel[name] <br> Description: $hotel[description] <br> parking: $hotel[parking] <br> Vote: $hotel[vote] <br> Distance to center: $hotel[distance_to_center]</li>";
+  <!-- CSS -->
+  <link rel="stylesheet" href="./css/style.css" />
+</head>
+
+<body>
+  <header>
+    <h1>PHP Hotels</h1>
+  </header>
+
+  <main>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Name</th>
+          <th scope="col">Description</th>
+          <th scope="col">Parking</th>
+          <th scope="col">Vote</th>
+          <th scope="col">Distance to center</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <?php
+          foreach ($hotels as $hotel) {
+            if ($hotel['parking'] == true) {
+              $parking_available = 'yes';
+            } else {
+              $parking_available = 'no';
+            }
+            echo
+              "<td>$hotel[name]</td>
+            <td>$hotel[description]</td>
+            <td>$parking_available</td>
+            <td>$hotel[vote]</td>
+            <td>$hotel[distance_to_center]</td>
+            </tr>";
           }
-        ?>
-      </ul>
-    </main>
+          ;
+          ?>
+      </tbody>
+    </table>
+  </main>
 
-    <script type="text/javascript" src="./javascript/scripts.js"></script>
-  </body>
+  <script type="text/javascript" src="./javascript/scripts.js"></script>
+</body>
+
 </html>
